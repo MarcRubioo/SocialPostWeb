9016f26df430ec9cca3e0465a1844aebe50ace4b
Pau Casas <pau.casas10@gmail.com>
2024-05-21 15:42:39 +0200

    calendario perfil

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,43 +1,25 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FullCalendarComponent } from '@fullcalendar/angular';
+import { Component, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-calendar',
-  template: '<full-calendar #calendarRef class="full-calendar" [options]="calendarOptions"></full-calendar>',
+  template: '<full-calendar class="full-calendar" [options]="calendarOptions"></full-calendar>',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  @ViewChild('calendarRef') calendarComponent: FullCalendarComponent;
   calendarOptions: CalendarOptions;
 
   ngOnInit(): void {
     this.calendarOptions = {
       plugins: [dayGridPlugin],
       initialView: 'dayGridMonth',
+      locale: 'es',
+      firstDay: 1,
       headerToolbar: {
         left: 'prev,next',
         center: 'title',
         right: ''
-      },
-      customButtons: {
-        prev: {
-          text: '&lt;', // Flecha izquierda
-          click: () => {
-            this.calendarComponent.getApi().prev();
-          }
-        },
-        next: {
-          text: '&gt;', // Flecha derecha
-          click: () => {
-            this.calendarComponent.getApi().next();
-          }
-        }
-      },
-      buttonText: {
-        prev: '&lt;', // Flecha izquierda
-        next: '&gt;'  // Flecha derecha
       }
     };
   }
